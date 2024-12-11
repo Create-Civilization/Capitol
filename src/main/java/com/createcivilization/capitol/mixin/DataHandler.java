bb24475137197feb2d8c85a1835c82988e88c878
AngerVon2468 <thomaschad0909@gmail.com>
2024-12-11 20:23:33 +1100

    Renaming and refactoring mixins

--- src/main/java/com/createcivilization/capitol/mixin/DataHandler.java
+++ src/main/java/com/createcivilization/capitol/mixin/DataHandler.java
@@ -12,25 +12,28 @@ import org.spongepowered.asm.mixin.injection.callback.*;
 import java.io.*;
 
 @SuppressWarnings("all")
-@Mixin(MinecraftServer.class)
-public abstract class DataSaverImpl {
-
-    @Inject(at = @At(value = "INVOKE", target = "Lnet/minecraft/server/players/PlayerList;saveAll()V", shift = At.Shift.BEFORE), method = "saveEverything")
-    private void autoSaveTeams(boolean p_195515_, boolean p_195516_, boolean p_195517_, CallbackInfoReturnable<Boolean> cir) throws IOException {
-        TeamUtils.saveTeams();
-    }
-
-    @Inject(at = @At(value = "INVOKE", target = "Lorg/slf4j/Logger;info(Ljava/lang/String;)V", shift = At.Shift.BEFORE, ordinal = 1), method = "stopServer")
-    private void saveTeams(CallbackInfo ci) throws IOException {
-        TeamUtils.saveTeams();
-    }
+public final class DataHandler {
 
     @Mixin(DedicatedServer.class)
-    public abstract static class DataSaverImplButWeSaveTheData {
+    public abstract static class DataLoaderImpl {
 
         @Inject(at = @At(value = "INVOKE", target = "Lnet/minecraftforge/common/ForgeConfigSpec$BooleanValue;get()Ljava/lang/Object;", shift = At.Shift.BEFORE), method = "initServer")
         public void loadTeams(CallbackInfoReturnable<Boolean> cir) throws IOException {
             TeamUtils.loadTeams();
         }
     }
+
+	@Mixin(MinecraftServer.class)
+	public abstract static class DataSaverImpl {
+
+		@Inject(at = @At(value = "INVOKE", target = "Lnet/minecraft/server/players/PlayerList;saveAll()V", shift = At.Shift.BEFORE), method = "saveEverything")
+		private void autoSaveTeams(boolean p_195515_, boolean p_195516_, boolean p_195517_, CallbackInfoReturnable<Boolean> cir) throws IOException {
+			TeamUtils.saveTeams();
+		}
+
+		@Inject(at = @At(value = "INVOKE", target = "Lorg/slf4j/Logger;info(Ljava/lang/String;)V", shift = At.Shift.BEFORE, ordinal = 1), method = "stopServer")
+		private void saveTeams(CallbackInfo ci) throws IOException {
+			TeamUtils.saveTeams();
+		}
+	}
 }
