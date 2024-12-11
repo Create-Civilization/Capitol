6dd2323293a2ceb351a1ad8cc30d14fcacaf152d
AngerVon2468 <thomaschad0909@gmail.com>
2024-12-11 14:23:20 +1100

    commiting the fixed version :3

--- src/main/java/com/createcivilization/capitol/command/custom/CreateTeamCommand.java
+++ src/main/java/com/createcivilization/capitol/command/custom/CreateTeamCommand.java
@@ -49,43 +49,36 @@ public class CreateTeamCommand extends AbstractTeamCommand {
 
     @Override
     public void register(CommandDispatcher<CommandSourceStack> dispatcher) {
-        ObjectHolder<String> name = new ObjectHolder<>();
         dispatcher.register(Commands.literal("capitolTeams").requires((command) -> {
             if (!command.isPlayer()) {
-                System.out.println("Failure");
                 command.sendFailure(Component.literal("You must be a player to execute this command!"));
                 return false;
             } else return !command.getLevel().isClientSide();
         }).executes((c) -> 1).then(Commands.literal(commandName).requires(this::canExecuteAllParams).executes((c) -> 1)
                 .then(Commands.argument("name", StringArgumentType.string())
-                        .executes((command) -> {
-                            System.out.println("Failure");
-                            name.set(StringArgumentType.getString(command, "name"));
-                            return 1;
-                        })
-                        .then(Commands.argument("color", StringArgumentType.string()))
-                        .executes((command) -> {
-                            String color = StringArgumentType.getString(command, "color");
-                            return new ObjectHolder<Player>().ifPresentOrElse(player -> {
-                                try {
-                                    System.out.println("Success");
-                                    var localName = name.get();
-                                    TeamUtils.loadedTeams.add(TeamUtils.createTeam(name.get(), player, (Color) Color.class.getDeclaredField(color).get(null)));
-                                    command.getSource().sendSuccess(() -> Component.literal("Created team '" + localName + "' with color '" + color + "'."), true);
-                                    return 1;
-                                } catch (Throwable e) {
-                                    System.out.println("Failure");
-                                    e.printStackTrace(System.out);
-                                    e.printStackTrace(System.err);
-                                    command.getSource().sendFailure(Component.literal("Invalid color: '" + color + "'!"));
-                                    return -1;
-                                }
-                            }, () -> {
-                                System.out.println("Failure");
-                                command.getSource().sendFailure(Component.literal("You must " + this.mustWhat + " to use this command."));
-                                return -1;
-                            });
-                        })
+                        .executes((command) -> 1)
+                        .then(Commands.argument("color", StringArgumentType.word())
+                                .executes((command) -> {
+                                    String color = StringArgumentType.getString(command, "color").toLowerCase();
+                                    String name = StringArgumentType.getString(command, "name");
+                                    return new ObjectHolder<Player>(command.getSource().getPlayer()).ifPresentOrElse(player -> {
+                                        try {
+                                            System.out.println("Success");
+                                            TeamUtils.loadedTeams.add(TeamUtils.createTeam(name, player, (Color) Color.class.getDeclaredField(color).get(null)));
+                                            command.getSource().sendSuccess(() -> Component.literal("Created team '" + name + "' with color '" + color + "'."), true);
+                                            return 1;
+                                        } catch (Throwable e) {
+                                            e.printStackTrace(System.out);
+                                            e.printStackTrace(System.err);
+                                            command.getSource().sendFailure(Component.literal("Invalid color: '" + color + "'!"));
+                                            return -1;
+                                        }
+                                    }, () -> {
+                                        System.out.println("Failure ;L");
+                                        command.getSource().sendFailure(Component.literal("You must " + this.mustWhat + " to use this command."));
+                                        return -1;
+                                    });
+                                }))
                 ))
         );
     }
