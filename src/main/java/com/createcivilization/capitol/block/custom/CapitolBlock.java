9729706150494867f7504de900e10904ad940620
AngerVon2468 <thomaschad0909@gmail.com>
2024-12-13 18:23:47 +1100

    Blockstates and r/USdefaultism

--- src/main/java/com/createcivilization/capitol/block/custom/CapitolBlock.java
+++ src/main/java/com/createcivilization/capitol/block/custom/CapitolBlock.java
@@ -2,36 +2,59 @@ package com.createcivilization.capitol.block.custom;
 
 import com.createcivilization.capitol.block.entity.CapitolBlockEntity;
 
-import net.minecraft.core.BlockPos;
+import net.minecraft.core.*;
+import net.minecraft.world.item.context.BlockPlaceContext;
 import net.minecraft.world.level.BlockGetter;
 import net.minecraft.world.level.block.*;
 import net.minecraft.world.level.block.entity.BlockEntity;
-import net.minecraft.world.level.block.state.BlockState;
+import net.minecraft.world.level.block.state.*;
+import net.minecraft.world.level.block.state.properties.*;
 import net.minecraft.world.phys.shapes.*;
 
+@SuppressWarnings("all")
 public class CapitolBlock extends BaseEntityBlock {
 
     public static final VoxelShape SHAPE = Block.box(0,0,0, 16,16,16);
 
+	public static final DirectionProperty FACING = BlockStateProperties.HORIZONTAL_FACING;
+
     public CapitolBlock(Properties properties) {
         super(properties);
+		this.registerDefaultState(this.getStateDefinition().any().setValue(FACING, Direction.NORTH));
     }
 
     @Override
-	@SuppressWarnings("all")
     public VoxelShape getShape(BlockState state, BlockGetter level, BlockPos pos, CollisionContext context) {
         return SHAPE;
     }
 
     @Override
-	@SuppressWarnings("all")
     public RenderShape getRenderShape(BlockState state) {
         return RenderShape.MODEL;
     }
 
     @Override
-	@SuppressWarnings("all")
     public BlockEntity newBlockEntity(BlockPos blockPos, BlockState blockState) {
         return new CapitolBlockEntity(blockPos, blockState);
     }
+
+	@Override
+	public BlockState getStateForPlacement(BlockPlaceContext ctx) {
+		return this.getStateDefinition().any().setValue(FACING, ctx.getHorizontalDirection().getOpposite());
+	}
+
+	@Override
+	public BlockState rotate(BlockState state, Rotation rotation) {
+		return state.setValue(FACING, rotation.rotate(state.getValue(FACING)));
+	}
+
+	@Override
+	public BlockState mirror(BlockState state, Mirror mirror) {
+		return state.rotate(mirror.getRotation(state.getValue(FACING)));
+	}
+
+	@Override
+	protected void createBlockStateDefinition(StateDefinition.Builder<Block, BlockState> builder) {
+		builder.add(FACING);
+	}
 }
