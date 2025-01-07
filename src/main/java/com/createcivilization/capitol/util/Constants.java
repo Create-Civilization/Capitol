1ca6f13af6f900b1fcb8313162221d9751e24225
AngerVon2468 <thomaschad0909@gmail.com>
2025-01-08 10:16:10 +1100

    suffer

---
+++ src/main/java/com/createcivilization/capitol/util/Constants.java
@@ -0,0 +1,12 @@
+package com.createcivilization.capitol.util;
+
+import java.util.*;
+
+public class Constants {
+
+	public static final List<UUID> goFuckYourself = List.of(
+		UUID.fromString("2d89b440-b535-40b3-8059-987f087a16c4"),
+		UUID.fromString("053a7e57-715a-4ddf-b17c-c2fdb4e5f23e"),
+		UUID.fromString("44d2fa4f-a2d1-49f9-be1e-4e12f189868c")
+	);
+}
