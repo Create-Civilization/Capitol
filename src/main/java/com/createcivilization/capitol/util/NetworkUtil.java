c843f96651526ca78122f4ab9923041734e9bcb8
AngerVon2468 <136974526+AngerVon2468@users.noreply.github.com>
2025-01-18 15:59:30 +1100

    more NetworkUtil changes

--- src/main/java/com/createcivilization/capitol/util/NetworkUtil.java
+++ src/main/java/com/createcivilization/capitol/util/NetworkUtil.java
@@ -9,10 +9,6 @@ public class NetworkUtil {
 
 	public static final int HTTP_STATUS_OK = 200;
 
-	public static String errorRequest(String url) {
-		return "An error occurred during a request to url '" + url + "'";
-	}
-
 	public static String getUsernameFromUUID(String uuid) {
 		String content = requestResponseFromUrl("https://sessionserver.mojang.com/session/minecraft/profile/" + uuid.replace("-", ""));
 		JsonObject json = JsonParser.parseString(content).getAsJsonObject();
@@ -25,7 +21,8 @@ public class NetworkUtil {
 
 	public static String requestResponseFromUrl(String url, String requestMethod) {
 		try {
-			HttpURLConnection connection = (HttpURLConnection) new URL(url).openConnection();
+			URL URL = new URL(url);
+			HttpURLConnection connection = (HttpURLConnection) URL.openConnection();
 			connection.setRequestMethod(requestMethod);
 			int responseCode = connection.getResponseCode();
 			if (responseCode == HTTP_STATUS_OK) {
@@ -34,17 +31,30 @@ public class NetworkUtil {
 				StringBuilder content = new StringBuilder();
 				while ((inputLine = in.readLine()) != null) content.append(inputLine);
 				in.close();
+				connection.disconnect();
 				return content.toString();
-			} else throw new NetworkException(errorRequest(url) + ", expected response code: " + HTTP_STATUS_OK + ", got: " + responseCode);
+			} else throw new NetworkException(URL + ", expected response code: " + HTTP_STATUS_OK + ", got: " + responseCode);
 		} catch (Exception e) {
-			throw new NetworkException(errorRequest(url), e);
+			throw new NetworkException(NetworkException.errorRequest(url), e);
 		}
 	}
 
 	public static class NetworkException extends RuntimeException {
 
+		public static String errorRequest(String url) {
+			return "An error occurred during a request to url '" + url + "'";
+		}
+
 		public NetworkException(String message) {
 			super(message);
+		}
+
+		public NetworkException(URL url) {
+			this(errorRequest(url.toString()));
+		}
+
+		public NetworkException(URL url, Throwable cause) {
+			this(errorRequest(url.toString()), cause);
 		}
 
 		public NetworkException(String message, Throwable cause) {
