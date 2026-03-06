cc4dc0ac0758690fa8725a50cfd73f28064566a5
banana-boye <bferraz1313@gmail.com>
2026-03-06 19:06:36 +0000

    Base branch

--- src/main/java/com/createcivilization/capitol/common/managers/ClaimManager.java
+++ src/main/java/com/createcivilization/capitol/common/managers/ClaimManager.java
@@ -1,18 +1,15 @@
 package com.createcivilization.capitol.common.managers;
 
-import com.createcivilization.capitol.common.modules.database.Database;
-import com.createcivilization.capitol.common.modules.database.DummyDatabase;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.level.ChunkPos;
 
 public class ClaimManager {
-	public static Database database = new DummyDatabase();
 
 	public static boolean isChunkClaimed(ChunkPos chunkPos) {
-		return database.hasChunkAt(chunkPos);
+		return DatabaseManager.database.hasChunkAt(chunkPos);
 	}
 
 	public static boolean playerHasPermissionInChunk(Player player, ChunkPos chunkPos) {
-		return database.hasPermissionInChunk(player, chunkPos);
+		return DatabaseManager.database.hasPermissionInChunk(player, chunkPos);
 	}
 }
--- src/main/java/com/createcivilization/capitol/common/managers/DatabaseManager.java
+++ src/main/java/com/createcivilization/capitol/common/managers/DatabaseManager.java
@@ -1,5 +1,9 @@
 package com.createcivilization.capitol.common.managers;
 
+import com.createcivilization.capitol.common.modules.database.Database;
+import com.createcivilization.capitol.common.modules.database.DummyDatabase;
+
 public class DatabaseManager {
 
+	public static Database database = new DummyDatabase();
 }
