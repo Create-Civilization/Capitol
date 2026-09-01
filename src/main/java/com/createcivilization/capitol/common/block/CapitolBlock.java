6fd85a0b8a5cb7fd7dbe81feafa547e28e4c92c5
starpanda24825 <starpanda24825@gmail.com>
2026-09-01 19:18:57 +0100

    fixed crash when capitol block is broken + render model correctly

--- src/main/java/com/createcivilization/capitol/common/block/CapitolBlock.java
+++ src/main/java/com/createcivilization/capitol/common/block/CapitolBlock.java
@@ -19,8 +19,10 @@ import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.context.BlockPlaceContext;
 import net.minecraft.world.level.ChunkPos;
 import net.minecraft.world.level.Level;
+import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.HorizontalDirectionalBlock;
 import net.minecraft.world.level.block.state.BlockState;
+import net.minecraft.world.level.block.state.StateDefinition;
 import net.minecraft.world.level.block.state.properties.BlockStateProperties;
 import net.minecraft.world.level.block.state.properties.DirectionProperty;
 import net.minecraft.world.phys.BlockHitResult;
@@ -42,6 +44,11 @@ public class CapitolBlock extends HorizontalDirectionalBlock {
     @Override
     protected MapCodec<? extends HorizontalDirectionalBlock> codec() {
         return simpleCodec(CapitolBlock::new);
+    }
+
+    @Override
+    protected void createBlockStateDefinition(StateDefinition.Builder<Block, BlockState> builder) {
+        builder.add(FACING);
     }
 
     @Override
